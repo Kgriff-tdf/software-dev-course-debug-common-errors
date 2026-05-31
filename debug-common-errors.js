4edff40ca352ac214d655939ca5ac6445e40a8ae
KGriffin <katie.griffin021@gmail.com>
2026-05-31 16:04:16 -0500

    Completed

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,23 +24,23 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
-
+// Syntax error It is missing the closing ', ), and ;
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
 // What’s Wrong?
-
+//Runtime error "eight" can not be multiplied
 
 
 // Program C (Logic Error)
@@ -51,12 +51,14 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+//Is labeled as a Logic Error. The returns in the for else statement are both opposite of what they 
+// should be to get a correct result.
